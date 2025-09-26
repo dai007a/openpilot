5772327254106e1bd3aaf5ba7c1cd800c5678afb
mooo758 <yccao94@gmail.com>
2025-09-26 14:12:16 +0800

    test-cp

--- selfdrive/ui/sunnypilot/qt/offroad/settings/lateral_panel.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/lateral_panel.cc
@@ -75,6 +75,16 @@ LateralPanel::LateralPanel(SettingsWindowSP *parent) : QFrame(parent) {
 
   list->addItem(horizontal_line());
 
+  // Pause Lateral on Steering Override
+  overridePauseLateralToggle = new ParamControl(
+    "PauseLateralOnOverride",
+    tr("Pause Lateral Control on Steering Override"),
+    tr("When enabled, lateral control will temporarily pause while the driver is applying steering torque (steering override)."),
+    "");
+  list->addItem(overridePauseLateralToggle);
+
+  list->addItem(horizontal_line());
+
   // Neural Network Lateral Control
   nnlcToggle = new NeuralNetworkLateralControl();
   list->addItem(nnlcToggle);
@@ -90,7 +100,7 @@ LateralPanel::LateralPanel(SettingsWindowSP *parent) : QFrame(parent) {
   });
 
   toggleOffroadOnly = {
-    madsToggle, nnlcToggle,
+    madsToggle, nnlcToggle, overridePauseLateralToggle,
   };
   QObject::connect(uiState(), &UIState::offroadTransition, this, &LateralPanel::updateToggles);
 
@@ -134,12 +144,16 @@ void LateralPanel::updateToggles(bool _offroad) {
   }
 
   auto cp_bytes = params.get("CarParamsPersistent");
-  if (!cp_bytes.empty()) {
+  auto cp_sp_bytes = params.get("CarParamsSPPersistent");
+  if (!cp_bytes.empty() && !cp_sp_bytes.empty()) {
     AlignedBuffer aligned_buf;
+    AlignedBuffer aligned_buf_sp;
     capnp::FlatArrayMessageReader cmsg(aligned_buf.align(cp_bytes.data(), cp_bytes.size()));
+    capnp::FlatArrayMessageReader cmsg_sp(aligned_buf_sp.align(cp_sp_bytes.data(), cp_sp_bytes.size()));
     cereal::CarParams::Reader CP = cmsg.getRoot<cereal::CarParams>();
+    cereal::CarParamsSP::Reader CP_SP = cmsg_sp.getRoot<cereal::CarParamsSP>();
 
-    if (isBrandInList(CP.getBrand(), mads_limited_settings_brands)) {
+    if (madsLimitedSettings(CP, CP_SP)) {
       madsToggle->setDescription(descriptionBuilder(STATUS_MADS_SETTINGS_LIMITED_COMPATIBILITY, MADS_BASE_DESC));
     } else {
       madsToggle->setDescription(descriptionBuilder(STATUS_MADS_SETTINGS_FULL_COMPATIBILITY, MADS_BASE_DESC));
