456005144b01b6296c768b09a1be536918da8fd1
mmoo758 <yccao94@gmail.com>
2025-10-04 19:07:30 +0800

    Revert "调整转向限制设置静音时不提醒，绿灯及前车起步静音式提醒"
    This reverts commit 6c2ad2dc85498736fbbe38a6cc2d9b9e68e8c211.

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -433,7 +433,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
     ET.WARNING: Alert(
       "请接管车辆",
       "超过转向限制",
-      AlertStatus.normal, AlertSize.mid,
+      AlertStatus.userPrompt, AlertSize.mid,
       Priority.LOW, VisualAlert.steerRequired, AudibleAlert.promptRepeat, 2.),
   },
 
