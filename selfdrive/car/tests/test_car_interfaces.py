1105955103ca8ae0aa7d063dd927aa56ea6d083c
mmoo758 <yccao94@gmail.com>
2025-12-06 09:30:30 +0800

    Revert "Lateral controllers: pass dt (delta time) explictly (#36281)"
    This reverts commit 2a8efcd8c6530766635c2da1548a1baa5ca7d553.

--- selfdrive/car/tests/test_car_interfaces.py
+++ selfdrive/car/tests/test_car_interfaces.py
@@ -103,8 +103,8 @@ class TestCarInterfaces:
     #  hypothesis also slows down significantly with just one more message draw
     LongControl(car_params)
     if car_params.steerControlType == CarParams.SteerControlType.angle:
-      LatControlAngle(car_params, car_params_sp, car_interface, DT_CTRL)
+      LatControlAngle(car_params, car_params_sp, car_interface)
     elif car_params.lateralTuning.which() == 'pid':
-      LatControlPID(car_params, car_params_sp, car_interface, DT_CTRL)
+      LatControlPID(car_params, car_params_sp, car_interface)
     elif car_params.lateralTuning.which() == 'torque':
-      LatControlTorque(car_params, car_params_sp, car_interface, DT_CTRL)
+      LatControlTorque(car_params, car_params_sp, car_interface)
