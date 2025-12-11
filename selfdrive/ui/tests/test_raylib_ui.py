40a7ea0569781d00b6d0e7d4e1676b7335d42c25
mmoo758 <yccao94@gmail.com>
2025-12-11 11:23:37 +0800

    Revert "raylib: flip! (#36319)"
    This reverts commit 9499939ef7bb5d1fd6dd54b515285f9c4e577fc6.

--- selfdrive/ui/tests/test_raylib_ui.py
+++ selfdrive/ui/tests/test_raylib_ui.py
@@ -2,7 +2,7 @@ import time
 from openpilot.selfdrive.test.helpers import with_processes
 
 
-@with_processes(["ui"])
+@with_processes(["raylib_ui"])
 def test_raylib_ui():
   """Test initialization of the UI widgets is successful."""
   time.sleep(1)
